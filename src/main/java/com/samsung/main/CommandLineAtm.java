57a28a2e3c579752bf0aaf63bd75ac8f0cd9e038
leeyounho <thestarrises@naver.com>
2024-09-18 20:08:09 +0900

    Initial commit

--- src/main/java/com/samsung/main/CommandLineAtm.java
+++ src/main/java/com/samsung/main/CommandLineAtm.java
@@ -4,12 +4,21 @@ import com.samsung.command.CommandProcessor;
 import com.samsung.command.Status;
 import com.samsung.command.component.CommandProcessorFactory;
 import com.samsung.command.component.DaggerCommandProcessorFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
 
 
 class CommandLineAtm {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
+        LOGGER.debug("Debug message");
+        LOGGER.info("Info message");
+        LOGGER.warn("Warn message");
+        LOGGER.error("Error message");
+
         Scanner scanner = new Scanner(System.in);
         CommandProcessorFactory commandProcessorFactory = DaggerCommandProcessorFactory.create();
         CommandProcessor commandProcessor = commandProcessorFactory.commandProcessor();
