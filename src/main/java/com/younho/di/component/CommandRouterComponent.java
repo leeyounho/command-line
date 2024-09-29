f7bfd23675db5194e5d661bf583cfa18d5ea5d01
leeyounho <thestarrises@naver.com>
2024-09-29 23:16:14 +0900

    backup

--- src/main/java/com/younho/di/component/CommandRouterComponent.java
+++ src/main/java/com/younho/di/component/CommandRouterComponent.java
@@ -3,7 +3,6 @@ package com.younho.di.component;
 import com.younho.command.CommandRouter;
 import com.younho.di.annotation.CommandScope;
 import com.younho.di.module.command.PicoCommandModule;
-import com.younho.di.module.command.HelloWorldModule;
 import dagger.Subcomponent;
 
 
@@ -11,7 +10,6 @@ import dagger.Subcomponent;
 @Subcomponent(
         modules = {
                 PicoCommandModule.class,
-                HelloWorldModule.class,
         })
 public interface CommandRouterComponent {
     CommandRouter router();
