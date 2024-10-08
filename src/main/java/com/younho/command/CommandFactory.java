888f5dbf852e9d20e78fe37bbd35befbf5fcf123
leeyounho <thestarrises@naver.com>
2024-10-09 01:23:31 +0900

    backup

--- src/main/java/com/younho/command/CommandFactory.java
+++ src/main/java/com/younho/command/CommandFactory.java
@@ -1,21 +1,33 @@
 package com.younho.command;
 
 import com.younho.cache.Cache;
+import com.younho.command.picocli.Ascii;
+import com.younho.command.picocli.Exit;
 import com.younho.command.picocli.HelloWorld;
+import com.younho.util.ConsoleLogger;
 
 import javax.inject.Inject;
 
 public class CommandFactory {
-    @Inject public Cache cache;
+    @Inject
+    public Cache cache;
+    @Inject
+    public ConsoleLogger console;
 
     @Inject
     public CommandFactory() {
     }
 
     public Command createCommand(String commandKey) {
-        if (commandKey.equals("picohello")) {
-            return new HelloWorld(cache);
+        switch (commandKey) {
+            case "ascii":
+                return new Ascii();
+            case "picohello":
+                return new HelloWorld(cache);
+            case "exit":
+                return new Exit(console);
+            default:
+                return null; // TODO execute 안되도록 handling 해야함
         }
-        return null;
     }
 }
